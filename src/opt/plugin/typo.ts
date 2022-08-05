16f70dff39698f289b9604b97a0bd89a145b71e9
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-08-05 23:17:28 +0900

    feat: :sparkles: add toAns

--- src/opt/plugin/typo.ts
+++ src/opt/plugin/typo.ts
@@ -1,4 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const Typo = require("typo-js");
 
-export const makeDictionary = (langCode: string) => new Typo(langCode);
+/**
+ * @refs https://github.com/cfinke/Typo.js
+ */
+export const makeDictionary = (p: { langCode: string }) => new Typo(p.langCode);
+export const toAns = (p: Array<string>) => {
+  if (p.length === 0) {
+    return true;
+  }
+  return p.join(", ");
+};
