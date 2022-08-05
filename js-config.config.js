78f8a9b96cc2b5abdab49519fea93e8a50726567
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-08-05 22:22:45 +0900

    fix: :bug: rm conf

--- js-config.config.js
+++ js-config.config.js
@@ -6,14 +6,5 @@ module.exports = {
       state: "open",
       assignUser: "akira-toriyama",
     },
-    scope: [
-      ...require("fs")
-        .readdirSync("./src")
-        .map((v) => ({
-          description: v,
-          value: v,
-        })),
-      { description: "other: Other directory", value: "other" },
-    ],
   },
 };
