61f53d7bbf5a8936a4b521307355b7076ff65492
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-08-06 20:51:51 +0900

    feat: :sparkles: add grammar

---
+++ src/opt/plugin/grammar.ts
@@ -0,0 +1,29 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const textgears = require("textgears-api");
+
+/**
+ * https://textgears.com/api
+ */
+export const cretaGrammarApi = (p: { key: string; language: string }) =>
+  textgears(p.key, { language: p.language });
+
+export const checkGrammar = (p: { grammarApi: any; txt: string }) =>
+  p.grammarApi
+    .checkGrammar(p.txt)
+    .then((data: any) => ({ isFailure: false, data }))
+    .catch((error: any) => ({ isFailure: true, error }))
+    .then((v: any) => {
+      if (v.isFailure) {
+        return v;
+      }
+
+      if (v.data.status) {
+        return v;
+      }
+
+      return v.data.response.errors.map((err: any) => ({
+        descriptionEn: err.description.en,
+        bad: err.bad,
+        better: err.better,
+      }));
+    });
